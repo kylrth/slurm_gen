97d80d01f51b4265110dd7b2a0613587a80cd691
Kyle Roth <kylrth@gmail.com>
2019-08-06 18:55:55 -0600

    added Cache object

--- slurm_gen/data_objects.py
+++ slurm_gen/data_objects.py
@@ -9,6 +9,8 @@ Kyle Roth. 2019-08-05.
 
 import os
 
+from slurm_gen import utils
+
 
 class Group:
     """Represents a group of data samples and all of their preprocessed versions."""
@@ -61,9 +63,40 @@ class Dataset:
     def __getitem__(self, idx):
         """Access either by name of ParamSet or by index."""
         if isinstance(idx, int):
-            return ParamSet(self.elements[idx])
+            return ParamSet(os.path.join(self.path, self.elements[idx]))
         if isinstance(idx, str):
             if idx not in self.elements:
                 raise ValueError('no such parameter set found: {}'.format(os.path.join(self.path, idx)))
             return ParamSet(os.path.join(self.path, idx))
         raise IndexError('ParamSet access is only permitted with int or str; got {}'.format(type(idx)))
+
+
+class Cache:
+    """Represents a cache location and all of its datasets."""
+    def __init__(self):
+        """For now, the cache location is constant."""
+        self.path = utils.get_cache_dir()
+        self.iterator = None
+        self.elements = list(sorted(os.listdir(self.path)))
+
+    def __iter__(self):
+        self.iterator = iter(self.elements)
+        return self
+
+    def __next__(self):
+        """Iterate over datasets in the cache.
+
+        Returns:
+            (Dataset): the next dataset in the cache.
+        """
+        return Dataset(os.path.join(self.path, next(self.iterator)))
+
+    def __getitem__(self, idx):
+        """Access either by name of Dataset or by index."""
+        if isinstance(idx, int):
+            return Dataset(os.path.join(self.path, self.elements[idx]))
+        if isinstance(idx, str):
+            if idx not in self.elements:
+                raise ValueError('no such dataset found: {}'.format(os.path.join(self.path, idx)))
+            return Dataset(os.path.join(self.path, idx))
+        raise IndexError('Dataset access is only permitted with int or str; got {}'.format(type(idx)))
