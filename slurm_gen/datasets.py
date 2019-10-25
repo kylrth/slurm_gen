c2457ec1a147adfa9e6111e021121cf386a7dde6
kylrth <kylrth@gmail.com>
2019-10-25 10:03:21 -0600

    noisy_sine dataset example

--- slurm_gen/datasets.py
+++ slurm_gen/datasets.py
@@ -233,6 +233,34 @@ def test__single_sim(**kwargs):
         plt.clf()
 
 
+class NoisySineParams(utils.DefaultParamObject):
+    """Attributes defining parameters to the noisy_sine experiment."""
+
+    # leftmost allowed value for x
+    left = -1
+
+    # rightmost allowed value for y
+    right = 1
+
+    # standard deviation of noise to add to sin(x)
+    std_dev = 0.1
+
+
+@generator(50, NoisySineParams)
+def noisy_sine(size, params):
+    """Create samples from a noisy sine wave.
+
+    Args:
+        size (int): number of samples to generate.
+        params (NoisySineParams): parameters to the experiment.
+    Yields:
+        (float): x-value.
+        (float): y-value plus noise.
+    """
+    for x in np.random.uniform(params.left, params.right, size=size):
+        yield x, np.sin(x) + np.random.normal(scale=params.std_dev)
+
+
 class WavelengthSweepParams(utils.DefaultParamObject):
     """Attributes defining parameters to the wavelength_sweep experiment."""
 
