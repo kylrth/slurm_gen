07f989c4dd1e64a8dc654832bddce1a859e8c2c1
Kyle Roth <kylrth@gmail.com>
2019-11-12 21:45:13 -0700

    TF imported first for supercomputer

--- slurm_gen/__init__.py
+++ slurm_gen/__init__.py
@@ -4,4 +4,7 @@ Kyle Roth. 2019-06-05.
 """
 
 
+# TensorFlow must be imported first in order to load correctly on the supercomputer
+import tensorflow as tf
+
 from slurm_gen.data_objects import Cache
